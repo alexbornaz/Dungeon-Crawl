c7fbfb827625fbb7bfafef1cc9d67432e9ba457a
Bornaz Alex <bornazalexg@gmail.com>
2022-12-27 14:58:57 +0200

    Sword and key item created

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -79,11 +79,13 @@ public class Main extends Application {
                 Cell cell = map.getCell(x, y);
                 if (cell.getActor() != null) {
                     Tiles.drawTile(context, cell.getActor(), x, y);
+                } else if (cell.getItem() != null) {
+                    Tiles.drawTile(context, cell.getItem(), x, y);
                 } else {
                     Tiles.drawTile(context, cell, x, y);
                 }
             }
+            healthLabel.setText("" + map.getPlayer().getHealth());
         }
-        healthLabel.setText("" + map.getPlayer().getHealth());
     }
 }
